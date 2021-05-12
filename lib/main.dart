a63a05c74ba14fd193d261566e982de3fca6a6c9
knoozfatima <knoozkk.edu@gmail.com>
2021-05-12 23:50:12 +0500

    Flutter First app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget
+{
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Name: Knooz Fatima   Email: knoozkk.edu@gmail.com"),
+      ),
+
+    );
+  }
+}
+
